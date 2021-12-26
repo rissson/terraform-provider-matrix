353a106480452d632fef6892ac7b383e796026e5
Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>
2021-12-27 00:00:23 +0100

    Initial commit
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@lama-corp.space>

---
+++ README.md
@@ -0,0 +1,6 @@
+# Terraform provider for Matrix (terraform-provider-matrix)
+
+## Documentation
+
+You can browse documentation on the [Terraform provider
+registry](https://registry.terraform.io/providers/rissson/matrix).
---
+++ examples/provider/provider.tf
@@ -0,0 +1,5 @@
+provider "matrix" {
+  homeserver_url = "https://matrix.example.org" # optionally use HOMESERVER_URL env var
+  username       = "my_user"                    # optionally use MATRIX_USERNAME env var
+  secret_key     = "verysecretpassword"         # optionally use MATRIX_PASSWORD env var
+}
